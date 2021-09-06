63c203089d888a174d76d40f09091c2f4aeb0e16
luk3skyw4lker <lucashenriqueblemos@gmail.com>
2021-09-06 14:45:07 -0300

    [test]: Add test case for multiple arguments to log

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,9 +1,11 @@
 const { Logger } = require('../dist');
 
+const object = { 123: 'testando' };
 try {
 	Logger.debug('MESSAGE');
 	Logger.warn('WARNING');
 	Logger.error('ERROR');
+	Logger.debug('1', 2, 3, object);
 } catch (error) {
 	process.exit(1);
 }
